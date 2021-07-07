c1b8ed16f232d7566c77301081a82652273b2e2e
Steve Gilissen <info@pulselogic.be>
2021-07-07 14:19:55 +0200

    Initial version

---
+++ scripts/SpotifyNotifierDBUS.sh
@@ -0,0 +1,4 @@
+# Fetch Spotify info via DBUS, show notification
+artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+track=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2`
+notify-send -i /home/sgilissen/gitdir/StreamdeckConfigs/GeneralIcon/spotify.png "Now Playing" "$artist - $track"
